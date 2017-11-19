f0eef314bf6a889e3829b40197b4ef5d0981ede2
Vitaliy Stoliarov <huccah@gmail.com>
2017-11-19 11:17:29 +0200

    UMD format

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,18 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'src/index.js',
-  dest: 'build/curve-editor.js',
-  plugins: [ 
-	   babel({
-	  "presets": [
-		[
-		  "es2015",
-		  {
-			"modules": false
-		  }
-		]
-	  ],
-	  plugins: ['external-helpers']
-})
-  ],
-  format: 'iife',
-  moduleName: "D3CE"
+    entry: 'src/index.js',
+    dest: 'build/d3-curve-editor.js',
+    plugins: [babel({
+        presets: [
+            [
+                'es2015', {
+                    'modules': false
+                }
+            ]
+        ],
+        plugins: ['external-helpers']
+    })],
+    format: 'umd',
+    moduleName: 'D3CE'
 };
